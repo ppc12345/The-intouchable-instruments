b3decddd9bd8d55cd26b0ccdd3ff69f2c2a823c5
Xian Zhang <2628107z@student.gla.ac.uk>
2022-04-18 00:57:39 +0100

    add utils.h

--- utils.h
+++ utils.h
@@ -1,1 +1,14 @@
-#
+#ifndef UTILS_H_
+#DEFINE UTILS_H_
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <QImage>
+
+namespace utils{
+    Image image=cvmat_toqimage(const cv::Mat& mat);
+
+}
+
+#endif
