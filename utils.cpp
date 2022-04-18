8a22cfa828149f5e3acc63dcf323d040fd44ad99
Xian Zhang <2628107z@student.gla.ac.uk>
2022-04-18 01:01:53 +0100

    add utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,1 +1,10 @@
-#
+#include "utils.h"
+
+namespace utils
+{
+    QImage cvmat_qimage(const cv::Mat& mat);
+    {
+        QImage image(mat, data, mat.cols,mat.rows,mat.step,QImage::Format_RGB888);
+        return image;
+    }
+}
